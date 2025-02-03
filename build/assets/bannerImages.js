c3b2d74e58a197fcbb928ff3dc94bf480352e6d4
SANDUN55 <sandunchinthakab55@gmail.com>
2025-02-03 13:49:31 +0530

    delete unnecessary files

--- build/assets/bannerImages.js
+++
@@ -1,9 +0,0 @@
-const images = {
-    'dune2': require("../assets/movie/backdrop/dune2.jpg"),
-    'riseOfRed': require("../assets/movie/backdrop/riseOfRed.jpg"),
-    'furiosa': require("../assets/movie/backdrop/furiosa.jpg"),
-    'planetApes': require("../assets/movie/backdrop/planetApes.jpg"),
-    'godzilla': require("../assets/movie/backdrop/godzilla.jpg")    
-};
-
-export default images;
--- build/assets/images.js
+++
@@ -1,2 +0,0 @@
-export {default as myflix} from './myflix.png';
-export {default as regularUser} from './users/regularUser.png';
